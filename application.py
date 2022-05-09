6cd3603c4e1eea3da3a6db2dd70de78dacadba91
Yousefelsharkawy <yousefelsharkawy111@gmail.com>
2022-05-09 05:10:05 +0200

    registration for staff members added and course registeration feauture added

--- application.py
+++ application.py
@@ -7,7 +7,9 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import login_required
+from helpers import login_required, apology
+
+
 
 # Configure application
 app = Flask(__name__)
@@ -30,19 +32,22 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure python standard library Library to use SQLite database
-con = sqlite3.connect('mymo.db')
-cur = con.cursor()
+con = sqlite3.connect('school.db',check_same_thread=False)
+db = con.cursor()
+
+def listify (obj):
+    """makes the output of the database SELECT as a list contains a dictionary"""
+    keys = []
+    lis = []
+    for i, n in enumerate(db.description):
+        keys.append(n[0])
+    for item in obj:
+        lis.append({keys[i]: item[i] for i in range(len(item))})
+    return lis
 
 
-def errorhandler(e):
-    """Handle error"""
-    if not isinstance(e, HTTPException):
-        e = InternalServerError()
-    return render_template("apology.html")
 
-# Listen for errors
-for code in default_exceptions:
-    app.errorhandler(code)(errorhandler)
+
 
 
 
@@ -51,45 +56,137 @@ for code in default_exceptions:
 @app.route("/", methods=["GET", "POST"])
 @login_required
 def index():
-    return 'hello world'
+    return render_template("index.html")
+
+
+@app.route("/homeStaff", methods=["GET", "POST"])
+@login_required
+def homestaff():
+    if request.method == "GET":
+        return render_template("index.html",msg = "home_staff",name=session["user_username"])
+    else:
+        course = request.form.get("add_course")
+        ass = request.form.get("add_assignment")
+        person = listify(db.execute("SELECT * FROM teachingStaff WHERE staffId = :id",{"id":session["user_id"]}) )
+        if course is not None:
+            courses = listify(db.execute("SELECT * FROM Courses WHERE department IN ( :department , 'asu' )",{"department":person[0]['department']  }) )
+            return render_template("index.html",msg = "courses_section",courses = courses)
+        else:
+            return apology("assignment selected")
+
+
+@app.route("/homeStudent", methods=["GET", "POST"])
+@login_required
+def homestudent():
+    if request.method == "GET":
+        return render_template("index.html",msg = "home_student",name=session["user_username"])
 
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
-        return render_template("register.html")
+        return render_template("register.html",msg = "register_student")
     else:
-        if not request.form.get("username"):
-            return apology("Please provide username!")
-        #Ensure that the user name does'nt already exists.
-        elif len(con.execute("SELECT * FROM users WHERE username =?",request.form.get("username"))):
-            return apology("Name already exist")
+        username = request.form.get("username")
+        fullname = request.form.get("fullname")
+        address = request.form.get("address")
+        dob = request.form.get("date")
+        nationality = request.form.get("nationality")
+        Id = request.form.get("id")
+        dept = request.form.get("department")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirm")
+        if fullname == "":
+            return apology("Please Enter your Full name")
+        if len(fullname) < 5:
+            return apology("your name should be at least 5 characters long")
+        if username == "":
+            return apology("Please Enter your account")
+        if len(username) < 5:
+            return apology("your Account should be at least 5 characters long")
+        users =  listify(db.execute("SELECT * FROM student WHERE username =:username", {"username":username}))
+        if len(users) != 0:
+            return apology('Email Already Exists!')
+        if address == "":
+            return apology("Please Enter your address")
+        if dob == "":
+            return apology("Please Enter your date of birth")
+        if nationality == "":
+            return apology("Please Choose your Nationality")
+        if Id == "":
+            return apology("Please Enter your national id or Passport Number")
+        if dept == "":
+            return apology("Please Choose Your Department")
+        if password == "":
+            return apology("Please Enter your Password")
+        if len(password) < 8:
+            return apology("Your Password Should Be At Least 8 Digits long")
+        if confirmation == "":
+            return apology("Please Re-Enter your password")
+        if  confirmation != password:
+            return apology("The Passwords Don't Match!")
         else:
-            username = request.form.get("username")
-
-        #Ensure password was submited
-        if not request.form.get("password"):
-            return apology("Please Provide Password!")
-        else:
-            password = request.form.get("password")
-
-        #Ensure confirm was submited
-        if not request.form.get("confirm"):
-            return apology("Please re-write the password!")
-
-        #Ensure password == confirm
-        else:
-            confirm = request.form.get("confirm")
-            if password != confirm:
-                return apology("passwords don't match!")
+            if nationality == 'egyptian':
+                db.execute('INSERT INTO student (name,address,dateOfBirth,nationality,nationalId,department,username,password) VALUES(:name,:address,:dateOfBirth,:nationality,:nationalID,:department,:username,:password)',{"name":fullname,"address":address,"dateOfBirth":dob,"nationality":nationality,"nationalID":Id,"department":dept,"username":username,"password":generate_password_hash(password)} )
+                con.commit()
             else:
-                id_primkey = con.execute("INSERT INTO users (username,hash) VALUES(?,?)",username, generate_password_hash(password))
-                # Remember which user has Reigsterd
-                session["user_id"] = id_primkey
+                db.execute('INSERT INTO student (name,address,dateOfBirth,nationality,passport,department,username,password) VALUES(:name,:address,:dateOfBirth,:nationality,:passport,:department,:username,:password)',{"name":fullname,"address":address,"dateOfBirth":dob,"nationality":nationality,"passport":Id,"department":dept,"username":username,"password":generate_password_hash(password)} )
+                con.commit()
 
-                #Redirect the user to home page
-                return redirect("/login")
+            user_data_list =  listify(db.execute("SELECT sid, username FROM student WHERE username =:username", {"username":username}))
+
+            # Remember which user has Reigsterd
+            session["user_id"] = user_data_list[0]["sid"]
+            session["user_username"] = user_data_list[0]["username"]
+
+            #Redirect the user to home page
+            return redirect("/login")
+
+
+@app.route("/register_staff", methods=["GET", "POST"])
+def register_staff():
+    if request.method == "GET":
+        return render_template("register.html",msg = "register_staff")
+    else:
+        fullname = request.form.get("fullname")
+        username = request.form.get("username")
+        degree = request.form.get("degree")
+        password = request.form.get("password")
+        confirm = request.form.get("confirm")
+        dept = request.form.get("department")
+
+        if fullname == "":
+            return apology("Please Enter your Full name")
+        if len(fullname) < 5:
+            return apology("your name should be at least 5 characters long")
+        if username == "":
+            return apology("Please Enter your account")
+        if len(username) < 5:
+            return apology("your Account should be at least 5 characters long")
+        users =  listify(db.execute("SELECT * FROM teachingStaff WHERE username =:username", {"username":username}))
+        if len(users) != 0:
+            return apology('Email Already Exists!')
+        if degree == "":
+            return apology("Please Select Your Degree")
+        if dept == "":
+            return apology("Please Choose Your Department")
+        if password == "":
+            return apology("Please Enter your Password")
+        if len(password) < 8:
+            return apology("Your Password Should Be At Least 8 Digits long")
+        if confirm == "":
+            return apology("Please Re-Enter your password")
+        if  confirm != password:
+            return apology("The Passwords Don't Match!")
+        else:
+            db.execute('INSERT INTO teachingStaff (name,degree,username,password,department) VALUES(:name,:degree,:username,:password,:department)',{"name":fullname,"degree":degree,"username":username,"password":generate_password_hash(password),"department":dept } )
+            con.commit()
+            #Redirect the user to home page
+        return redirect("/login")
+
+
+
 
 
 
@@ -104,22 +201,46 @@ def login():
     if request.method == "POST":
 
         # Ensure username was submitted
-        if not request.form.get("username"):
+        username = request.form.get("username")
+        password = request.form.get("password")
+        Type = request.form.get("type")
+        if username == "" :
             return apology("must provide username")
+        if password == "":
+            return apology("must provide Password")
+        if Type == "":
+            return apology("Please Select The Type")
 
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password")
 
         # Query database for username
+        ''' اسم الداتا بيز الي انت حاطه غبي YA'''
+        if Type == 'student':
+            rows = listify(db.execute("SELECT * FROM student WHERE username = :username",{"username":request.form.get("username")}) )
+            # Remember which user has logged in
+            session["user_id"] = rows[0]["sid"]
+            session["user_username"] = rows[0]["name"]
 
-        rows = con.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password")
+            # Ensure username exists and password is correct
+            if len(rows) != 1 or not check_password_hash(rows[0]["password"], request.form.get("password")):
+                return apology("invalid username and/or password")
+            # Redirect user to home page
+            return redirect("/homeStudent")
+        else:
+            rows = listify(db.execute("SELECT * FROM teachingStaff WHERE username = :username",{"username":request.form.get("username")}) )
+            # Remember which user has logged in
+            session["user_id"] = rows[0]["staffId"]
+            session["user_username"] = rows[0]["name"]
 
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+            # Ensure username exists and password is correct
+            if len(rows) != 1 or not check_password_hash(rows[0]["password"], request.form.get("password")):
+                return apology("invalid username and/or password")
+            # Redirect user to home page
+            return redirect("/homeStaff")
+
+
+
+
+
 
         # Redirect user to home page
         return redirect("/")
@@ -146,3 +267,13 @@ def logout():
 
 
 
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return render_template("apology.html")
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
